c8a458a65691ea1a4363629cfcc4becaec5ef474
Navin Lalwani <navin@MacBook-Air-4.local>
2020-04-14 17:33:36 +0530

    Updates

---
+++ precache-manifest.6c3cf98387e0292de4105cde4bf1c8c0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d073fb75fd2deae7e63e3d5ed2bbb7c9",
+    "url": "/smartbrain/index.html"
+  },
+  {
+    "revision": "3a7a35051b3d1a01e02c",
+    "url": "/smartbrain/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "47f352460bbd3899ce57",
+    "url": "/smartbrain/static/css/main.325de78e.chunk.css"
+  },
+  {
+    "revision": "3a7a35051b3d1a01e02c",
+    "url": "/smartbrain/static/js/2.bdcdbd98.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/smartbrain/static/js/2.bdcdbd98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47f352460bbd3899ce57",
+    "url": "/smartbrain/static/js/main.41b007a8.chunk.js"
+  },
+  {
+    "revision": "91d4664dec07545371ba",
+    "url": "/smartbrain/static/js/runtime-main.e2c70a70.js"
+  },
+  {
+    "revision": "e97d42e932971ec05ebf6052aff135e9",
+    "url": "/smartbrain/static/media/brain.e97d42e9.png"
+  }
+]);
